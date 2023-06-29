db75214dc30bd7b98eb5fb2f90d3bbbebe0c93ee
Davi Souza <124102479+Danvilique@users.noreply.github.com>
2023-06-29 19:48:24 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+
+var cenarioLOBBY;
+
+function preload(){
+
+    cenarioLOBBY = loadImage("LOBBY.png");
+
+}
+
+function setup(){
+
+    createCanvas(1000,500);
+
+}
+
+function draw(){
+
+background("blue");
+console.log("oi");
+
+drawSprites();
+
+}
